51d3dcd1420307f9925e9315875a413e3e92d408
Ruth Tingi <chepletingruth04@gmail.com>
2025-06-26 20:56:52 +0300

    ...

--- prediction_app/models.py
+++ prediction_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class PatientRecord(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    glucose = models.FloatField()
+    sysBP = models.FloatField()
+    diaBP = models.FloatField()
+    BMI = models.FloatField()
+    smoker = models.BooleanField()
+    prediction = models.CharField(max_length=20)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.prediction}"
